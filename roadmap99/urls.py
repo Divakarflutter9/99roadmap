4c37a4c65520610310efe360679d2ad18194485a
sai teja kaki <saitejakaki@sais-MacBook-Air.local>
2026-01-25 19:09:52 +0530

    Add sitemap.xml and robots.txt for SEO

--- roadmap99/urls.py
+++ roadmap99/urls.py
@@ -20,3 +20,11 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# SEO Files
+from django.views.generic import TemplateView
+
+urlpatterns += [
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
+]
